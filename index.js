2ad9ad031083082bb4af91318077d4bcff7e454b
Jean-Louis Leysens <jloleysens@gmail.com>
2017-08-05 10:46:22 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,3 @@
+const ESLINTRC = require('./.eslintrc');
+
+module.exports = ESLINTRC;
---
+++ test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const A = 'fish';
+const B = 'turnip';
+
+if (require.main == module) {
+  if (A == B) {
+    console.log('yaay!');
+  }
+}
